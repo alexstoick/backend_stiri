0375df16e8823af31d65e2078b631a48dc81726f
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-09-03 10:20:41 +0300

    cleaned up the controller + removed old unused action

--- app/controllers/newssource_controller.rb
+++ app/controllers/newssource_controller.rb
@@ -7,46 +7,7 @@ class NewssourceController < ApplicationController
 	#deleste un feed al userului -- will be solved cand impunem auth_token
 
 	def index
-
-		key = params[:key]
-		encrypted = params[:token]
-		decripted_key = Base64.decode64( key )
-
-		encrypted="Vvaz86qhQm1T2t9SXwvwlMRu+cAKPywUBQYvBxhAgYOZzoWGEJ0heeNY37V5V1J2zTRjsqoDNUjsRkQ0RDHDL03Kh51EefDNpWlzlxb0UsycXkQt2P+6FChzjlpvzVlFSY0Pz4MC3rxq33IU8IvboswiXlKjUkK0FdLADBmk+S99b4yZXrt1HNWlvMeQso9d4JfRC5krvvWTU0FhYDHS3PQlrrLmRulGgxqm1F/nn/n
-FX6z+7GNWxab/uJYdilv1Pjyr6HVm+uT2/d6gkFaZwkJusArypuH1TWI449+pZXCfrEpqEI3LQIjPzedmjfFrcweOV/2BMR8W+Bwc+f2/Qw=="
-
-		user = User.find_by_public_key ( decripted_key )
-
-		puts user.id
-		puts encrypted
-
-		private_key = OpenSSL::PKey::RSA.new( user.private_key , ENV['SECRET_KEY'] )
-		decripted_string = private_key.private_decrypt(Base64.decode64(encrypted))
-
-		render json: { "result" => decripted_string }
-		return
-
 		@feeds = Newssource.all
-	end
-
-	def change
-
-		new_group = params[:new_groupid]
-		current_device = params[:device]
-		feedid = params[:feedid]
-
-		feed = Newssource.find( feedid )
-		feed.newsgroup_id = new_groupid
-		feed.save!
-
-		options = {
-			title: "Schimbare grup feed"
-		}
-
-		#view_context.updateDevices(params[:id] , current_device , options )
-
-		render json: { "success" => true }
-
 	end
 
 	def delete
--- db/migrate/20130902095449_add_public_key_and_private_key_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddPublicKeyAndPrivateKeyToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :public_key, :text
-    add_column :users, :private_key, :text
-  end
-end
