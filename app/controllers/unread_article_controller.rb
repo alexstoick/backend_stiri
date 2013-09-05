e64396984ce043461ebdbcfc195156a3812ad97c
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-09-05 14:36:15 +0300

    added checks

--- app/controllers/unread_article_controller.rb
+++ app/controllers/unread_article_controller.rb
@@ -1,9 +1,9 @@
 class UnreadArticleController < ApplicationController
 
-	#before_filter :check_auth_token
+	skip_before_filter :verify_authenticity_token
+	before_filter :check_auth_token
 
 	def index
-		#trebuie returnat article_id
 		user = User.find(params[:id])
 		newssources = user.newssources.pluck(:id)
 		articles = user.unread_articles.where(newsgroup_id: newssources).pluck(:article_id)
