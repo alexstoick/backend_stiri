8d97dac16451c6783b7c696eecba201b9a3dd11f
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-09-07 18:25:27 +0300

    returning now ordered by subscribers

--- app/controllers/newssource_controller.rb
+++ app/controllers/newssource_controller.rb
@@ -6,7 +6,7 @@ class NewssourceController < ApplicationController
 	before_filter :check_group_holder , :except => [ :index, :select ]
 
 	def index
-		@feeds = Newssource.select('url, id, category, title, image').all
+		@feeds = Newssource.select('url, id, category, title, image, subscribers').order('subscribers DESC').all
 		@categories = Newssource.uniq.pluck(:category)
 	end
 
